a75a50b74e8ac44b319dc99eab495cd2826326f5
Sarah <117588659+Sawa99@users.noreply.github.com>
2025-05-29 16:52:40 +0100

    user input onto created boxes

--- public/script.js
+++ public/script.js
@@ -4,7 +4,8 @@ function create() {
     var holder = document.createElement("p");
     holder.setAttribute('class', 'output');
     // You can set the inner text of the p tag without creating a text node.
-    holder.innerText = "The text in the box"
+    var text = document.getElementById("task").value;
+    holder.innerText = text;
     box.appendChild(holder);
     // Trades should be an element with and ID because you probably only ever want to insert into one place.
     var tasks = document.getElementById("tasks");
